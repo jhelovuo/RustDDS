5c54977255bd3e617bc5a8eb1c6bd2f2698817e3
Klapeyron <tomasz.bartos@protonmail.com>
2018-02-06 12:45:57 +0100

    Changed into from/into trait

--- src/common/time.rs
+++ src/common/time.rs
@@ -1,5 +1,6 @@
 extern crate time;
 
+use std::convert::{From, Into};
 use self::time::Timespec;
 
 #[derive(Debug, Serialize, Deserialize, PartialOrd, PartialEq, Ord, Eq)]
@@ -13,18 +14,22 @@ pub const TIME_INVALID: Time_t = Time_t { seconds: -1, fraction: 0xFFFFFFFF };
 pub const TIME_INFINITE: Time_t = Time_t { seconds: 0x7FFFFFFF, fraction: 0xFFFFFFFF };
 
 impl Time_t {
-    pub fn new(timespec: time::Timespec) -> Time_t {
+    pub fn value(&self) -> i64 {
+        self.seconds as i64 + ((self.fraction as i64) << 32)
+    }
+}
+
+impl From<time::Timespec> for Time_t {
+    fn from(timespec: time::Timespec) -> Self {
         Time_t {
             seconds: timespec.sec as i32,
             fraction: timespec.nsec as u32
         }
     }
+}
 
-    pub fn value(&self) -> i64 {
-        self.seconds as i64 + ((self.fraction as i64) << 32)
-    }
-
-    pub fn to_timespec(&self) -> time::Timespec {
+impl Into<time::Timespec> for Time_t {
+    fn into(self) -> time::Timespec {
         time::Timespec {
             sec: self.seconds as i64,
             nsec: self.fraction as i32
