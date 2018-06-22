d3f41db67f9953c7827782107f0deaeae93c696f
Klapeyron <tomasz.bartos@protonmail.com>
2018-06-22 18:50:06 +0200

    FragmentNumber_t starts from one

--- src/common/fragment_number.rs
+++ src/common/fragment_number.rs
@@ -1,4 +1,12 @@
-#[derive(PartialOrd, PartialEq, Ord, Eq)]
+#[derive(Debug, PartialOrd, PartialEq, Ord, Eq)]
 pub struct FragmentNumber_t {
-    pub vendorId: u32
+    pub vendor_id: u32
+}
+
+impl Default for FragmentNumber_t {
+    fn default() -> FragmentNumber_t {
+        FragmentNumber_t {
+            vendor_id: 1
+        }
+    }
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,4 +1,5 @@
 mod count_test;
+mod fragment_number_test;
 mod protocol_id_test;
 mod sequence_number_test;
 mod submessage_flag_test;
