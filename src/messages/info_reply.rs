2cd45a01f15afe751586182c0f58c1ad6394d21e
Klapeyron <tomasz.bartos@protonmail.com>
2019-01-03 12:38:35 +0100

    Docs for InfoReply

--- src/messages/info_reply.rs
+++ src/messages/info_reply.rs
@@ -5,6 +5,14 @@ use crate::structure::locator::LocatorList_t;
 /// to the Submessages that follow it within the same message.
 #[derive(Debug, PartialEq)]
 pub struct InfoReply {
+    /// Indicates an alternative set of unicast addresses that
+    /// the Writershould use to reach the Readers when
+    /// replying to the Submessages that follow.
     pub unicast_locator_list: LocatorList_t,
-    pub multicast_locator_list: LocatorList_t,
+
+    /// Indicates an alternative set of multicast addresses that the Writer
+    /// should use to reach the Readers when replying to the Submessages that follow.
+    ///
+    /// Only present when the MulticastFlag is set.
+    pub multicast_locator_list: Option<LocatorList_t>,
 }
