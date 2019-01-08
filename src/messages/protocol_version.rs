43ada5920108774c16fa6b9a17cc364d8fdc337f
Klapeyron <tomasz.bartos@protonmail.com>
2019-01-08 21:38:17 +0100

    Support for RTPS 2.3

--- src/messages/protocol_version.rs
+++ src/messages/protocol_version.rs
@@ -14,6 +14,7 @@ impl ProtocolVersion_t {
     pub const PROTOCOLVERSION_2_0: ProtocolVersion_t = ProtocolVersion_t { major: 2, minor: 0 };
     pub const PROTOCOLVERSION_2_1: ProtocolVersion_t = ProtocolVersion_t { major: 2, minor: 1 };
     pub const PROTOCOLVERSION_2_2: ProtocolVersion_t = ProtocolVersion_t { major: 2, minor: 2 };
+    pub const PROTOCOLVERSION_2_3: ProtocolVersion_t = ProtocolVersion_t { major: 2, minor: 3 };
 }
 
 impl Default for ProtocolVersion_t {
@@ -68,5 +69,11 @@ mod tests {
         ProtocolVersion_t::PROTOCOLVERSION_2_2,
         le = [0x02, 0x02],
         be = [0x02, 0x02]
+    },
+    {
+        protocol_version_2_3,
+        ProtocolVersion_t::PROTOCOLVERSION_2_3,
+        le = [0x02, 0x03],
+        be = [0x02, 0x03]
     });
 }
