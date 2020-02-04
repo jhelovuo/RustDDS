e3c66891aec1f319d95241df1051a8fb5dad94a6
Tomasz Bartos <tomasz.bartos@protonmail.com>
2020-02-04 18:54:15 +0100

    Extended submessages types

--- src/messages/submessage.rs
+++ src/messages/submessage.rs
@@ -1,5 +1,6 @@
 use crate::messages::ack_nack::AckNack;
 use crate::messages::data::Data;
+use crate::messages::data_frag::DataFrag;
 use crate::messages::gap::Gap;
 use crate::messages::heartbeat::Heartbeat;
 use crate::messages::heartbeat_frag::HeartbeatFrag;
@@ -8,23 +9,24 @@ use crate::messages::info_reply::InfoReply;
 use crate::messages::info_source::InfoSource;
 use crate::messages::info_timestamp::InfoTimestamp;
 use crate::messages::nack_frag::NackFrag;
+use crate::messages::submessage_flag::SubmessageFlag;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum EntitySubmessage {
-    AckNack(AckNack),
-    Data(Data),
-    // DataFrag(DataFrag),
+    AckNack(AckNack, SubmessageFlag),
+    Data(Data, SubmessageFlag),
+    DataFrag(DataFrag, SubmessageFlag),
     Gap(Gap),
-    Heartbeat(Heartbeat),
+    Heartbeat(Heartbeat, SubmessageFlag),
     HeartbeatFrag(HeartbeatFrag),
     NackFrag(NackFrag),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum InterpreterSubmessage {
     InfoSource(InfoSource),
     InfoDestination(InfoDestination),
-    InfoReply(InfoReply),
-    InfoTimestamp(InfoTimestamp),
+    InfoReply(InfoReply, SubmessageFlag),
+    InfoTimestamp(InfoTimestamp, SubmessageFlag),
     // Pad(Pad),
 }
