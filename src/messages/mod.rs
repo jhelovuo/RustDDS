6fc162bde7a454fa72e7f36290181e2e6b49e176
Juhana Helovuo <juhana.helovuo@atostek.com>
2020-09-03 09:09:52 +0300

    Removed messages::receiver module as unused.

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -7,9 +7,7 @@ pub mod submessages;
 pub mod fragment_number;
 pub mod fragment_number_set;
 
-//pub mod pad;
 pub mod protocol_id;
 pub mod protocol_version;
-pub mod receiver;
 
 pub mod vendor_id;
