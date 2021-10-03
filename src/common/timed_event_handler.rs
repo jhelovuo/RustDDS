fce74887e9aabfbd788e93dd60aed2e1b9f06ef6
Daniel Eades <danieleades@hotmail.com>
2021-10-03 13:27:41 +0100

    use map 'entry' api

--- src/common/timed_event_handler.rs
+++ src/common/timed_event_handler.rs
@@ -28,9 +28,7 @@ impl<'a> TimedEventHandler {
   }
 
   pub fn set_timeout(&mut self, duration: &'a chrono::Duration, timer_type: TimerMessageType) {
-    if !self.timers.contains_key(&timer_type) {
-      self.timers.insert(timer_type, Timer::new());
-    }
+    self.timers.entry(timer_type).or_insert_with(Timer::new);
 
     let new_chanenel = self.channel_send.clone();
     self.guards.insert(
