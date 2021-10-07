3d1eab9d76fad8865263aa7ab983c331ebca766d
Daniel Eades <danieleades@hotmail.com>
2021-10-07 16:12:59 +0100

    remove redundant imports

--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,6 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-// extern crate bit_set;
-// extern crate bit_vec;
-extern crate bytes;
-extern crate num_derive;
-extern crate num_traits;
-extern crate speedy;
-extern crate tokio_util;
-extern crate uuid;
-
 #[macro_use]
 mod serialization_test;
 #[macro_use]
