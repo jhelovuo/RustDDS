a0ea9c85191e595bd8aec0930c68fcc640cadd2f
Daniel Eades <danieleades@hotmail.com>
2021-11-10 18:52:24 +0000

    remove 'EndpointAttributes' struct

--- src/structure/endpoint.rs
+++ src/structure/endpoint.rs
@@ -1,52 +1,12 @@
 use speedy::{Readable, Writable};
 use serde::{Deserialize, Serialize};
 
-use crate::structure::{locator::Locator, topic_kind};
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Readable, Writable, Serialize, Deserialize)]
 pub struct ReliabilityKind(u32);
 
 impl ReliabilityKind {
   pub const BEST_EFFORT: ReliabilityKind = ReliabilityKind(1);
   pub const RELIABLE: ReliabilityKind = ReliabilityKind(2);
-}
-
-/// Specialization of RTPS Entity representing the objects that can be
-/// communication endpoints. That is, the objects that can be the sources or
-/// destinations of RTPS messages.
-#[derive(Debug, PartialEq)]
-pub struct EndpointAttributes {
-  /// Used to indicate whether the Endpoint is associated with a DataType that
-  /// has defined some fields as containing the DDS key.
-  pub topic_kind: topic_kind::TopicKind,
-
-  /// The levels of reliability supported by the Endpoint.
-  pub reliability_level: ReliabilityKind,
-
-  /// List of unicast locators (transport, address, port combinations) that
-  /// can be used to send messages to the Endpoint. The list may be empty.
-  pub unicast_locator_list: Vec<Locator>,
-
-  /// List of multicast locators (transport, address, port combinations) that
-  /// can be used to send messages to the Endpoint. The list may be empty.
-  pub multicast_locator_list: Vec<Locator>,
-}
-
-impl EndpointAttributes {
-  fn new() -> EndpointAttributes {
-    EndpointAttributes {
-      topic_kind: topic_kind::TopicKind::NoKey,
-      reliability_level: ReliabilityKind::BEST_EFFORT,
-      unicast_locator_list: Vec::default(),
-      multicast_locator_list: Vec::default(),
-    }
-  }
-}
-
-impl Default for EndpointAttributes {
-  fn default() -> EndpointAttributes {
-    EndpointAttributes::new()
-  }
 }
 
 #[cfg(test)]
