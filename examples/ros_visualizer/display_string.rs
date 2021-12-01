19a13ea94c813e91ed7c38db9bcb7221a7337f6f
Miska Melkinen <melkinen@cruu.atostek-main.atostek.com>
2021-12-01 17:41:11 +0200

    Ros visualizer example can show topic data

--- examples/ros_visualizer/display_string.rs
+++ examples/ros_visualizer/display_string.rs
@@ -1,9 +1,10 @@
-
-//mod display_string{
 use rustdds::ros2::RosParticipant;
+use rustdds::dds::data_types::DiscoveredTopicData;
+use rustdds::ros2::builtin_datatypes::NodeInfo;
 
 
 
+/*
 pub fn clear_screen(){
   print!("{}[2J", 27 as char);
   print!("\x1B[2J\x1B[1;1H");
@@ -23,12 +24,15 @@ pub fn print_datas(participant : &RosParticipant){
   print_external_node_infos(participant);
 }
 
+
 pub fn print_topics(participant : &RosParticipant){
-  let strings = get_topics_view_strings(participant);
+  
+  let strings = get_topics_list_view_strings(&participant.discovered_topics());
   for s in strings{
     println!("  {:?}",s);
   }
 }
+
 
 pub fn print_local_participant_info(participant : &RosParticipant){
   let strings = get_local_ros_participant_info_strings(participant);
@@ -43,6 +47,7 @@ pub fn print_external_node_infos(participant : &RosParticipant){
     println!("  {:?}",s);
   }
 }
+*/
 
 pub fn get_local_ros_participant_info_strings(participant : &RosParticipant) -> Vec<String>{
   let info = participant.get_ros_participant_info();
@@ -51,11 +56,45 @@ pub fn get_local_ros_participant_info_strings(participant : &RosParticipant) -> 
   strings
 }
 
-pub fn get_topics_view_strings(participant : &RosParticipant) -> Vec<String>{
+
+
+pub fn get_topics_list_view_strings(discovered_topic_datas : &Vec<DiscoveredTopicData>) -> Vec<String>{
+  let mut strings = vec!();
+  for topic in discovered_topic_datas{
+    strings.push(format!("{:?}",topic.topic_name()));
+  }
+  strings
+}
+
+pub fn get_topic_view_strings(participant : &RosParticipant, topic_name : &String) -> Vec<String>{
   let topics = participant.discovered_topics();
   let mut strings = vec!();
-  for topic in topics{
-    strings.push(format!("name: {:?} type: {:?} ",topic.topic_name(), topic.type_name()));
+  match topics.into_iter().find(|x| x.topic_name() == topic_name){
+      //match ok{
+        Some(topic) =>{
+          strings.push(format!("name: {:?}", topic.topic_name()));
+          strings.push(format!("type_name: {:?}", topic.type_name()));
+          strings.push(format!("durability: {:?}", topic.topic_data.durability));
+          strings.push(format!("deadline: {:?}", topic.topic_data.deadline));
+          strings.push(format!("latency_budget: {:?}", topic.topic_data.latency_budget));
+          strings.push(format!("liveliness: {:?}", topic.topic_data.liveliness));
+          strings.push(format!("reliability: {:?}", topic.topic_data.reliability));
+          strings.push(format!("lifespan: {:?}", topic.topic_data.lifespan));
+          strings.push(format!("destination_order: {:?}", topic.topic_data.destination_order));
+          strings.push(format!("presentation: {:?}", topic.topic_data.presentation));
+          strings.push(format!("history: {:?}", topic.topic_data.history));
+          strings.push(format!("resource_limits: {:?}", topic.topic_data.resource_limits));
+          strings.push(format!("ownership: {:?}", topic.topic_data.ownership));  
+        }
+        None => {}
+      }
+  strings
+}
+
+pub fn get_node_list_strings(nodes : &Vec<NodeInfo>) -> Vec<String>{
+  let mut strings = vec!();
+  for node in nodes{
+    strings.push(format!("{:?}{:?}", node.namespace(), node.name()));
   }
   strings
 }
@@ -63,7 +102,7 @@ pub fn get_topics_view_strings(participant : &RosParticipant) -> Vec<String>{
 pub fn get_external_node_info_strings(participant : &RosParticipant) -> Vec<String> {
   let node_infos = participant.get_all_discovered_external_ros_node_infos();
   let mut strings = vec!();
-  for (gid, info_vec) in node_infos {
+  for (_gid, info_vec) in node_infos {
     for node_info in info_vec{
       strings.push(format!("name: {:?} namespace {:?} ",node_info.name(),node_info.namespace()   ));
     }
@@ -72,4 +111,3 @@ pub fn get_external_node_info_strings(participant : &RosParticipant) -> Vec<Stri
 }
 
 
-//}
--- examples/ros_visualizer/main.rs
+++ examples/ros_visualizer/main.rs
@@ -2,46 +2,61 @@ use std::{
   io,
   error::Error,
   time::{Duration, Instant},
+  collections::{HashMap}
 };
-use std::{thread, time, error};
 
-use log::{debug, error, trace, LevelFilter};
+use log::{LevelFilter};
 use log4rs::{
   append::console::ConsoleAppender,
   config::{Appender, Root},
   Config,
 };
 
+
+mod stateful_list;
+use crate::stateful_list::StatefulList;
 mod display_string;
-use crate::display_string::get_topics_view_strings;
+use crate::display_string::get_topics_list_view_strings;
+use crate::display_string::get_topic_view_strings;
 use crate::display_string::get_external_node_info_strings;
 use crate::display_string::get_local_ros_participant_info_strings;
+use crate::display_string::get_node_list_strings;
+mod visualization_helpers;
+use crate::visualization_helpers::create_paragraph_from_string_list;
+use crate::visualization_helpers::create_layput_row;
 
 
 
 
 
 use rustdds::dds::{
-  data_types::{DDSDuration, TopicKind},
-  qos::{
-    policy::{Deadline, Durability, History, Reliability},
-    QosPolicyBuilder,
-  },
-  statusevents::StatusEvented,
-  traits::{Keyed, TopicDescription},
+  //data_types::{DDSDuration, TopicKind},
+  //qos::{
+    //policy::{Deadline, Durability, History, Reliability},
+    //QosPolicyBuilder,
+  //},
+  //statusevents::StatusEvented,
+  //traits::{Keyed, TopicDescription},
+  
   DomainParticipant,
 };
-use serde::{Deserialize, Serialize};
-use clap::{App, Arg, ArgMatches}; // command line argument processing
+//use serde::{Deserialize, Serialize};
+//use clap::{App, Arg, ArgMatches}; // command line argument processing
 //use mio::{Events, Poll, PollOpt, Ready, Token}; // polling
 //use mio_extras::channel; // pollable channel
 //use rand::prelude::*;
 
 
-use rustdds::dds::traits::RTPSEntity;
+//use rustdds::dds::traits::RTPSEntity;
 use rustdds::ros2::RosParticipant;
 use rustdds::ros2::NodeOptions;
 use rustdds::ros2::RosNode;
+use rustdds::ros2::builtin_datatypes::NodeInfo;
+use rustdds::ros2::builtin_datatypes::Gid;
+
+use rustdds::dds::data_types::DiscoveredTopicData;
+
+
 //use rustdds::ros2::builtin_datatypes::NodeInfo;
 //use rustdds::dds::qos::QosPolicies;
 //use rustdds::serialization::CDRSerializerAdapter;
@@ -57,146 +72,148 @@ use tui::{
   layout::{Constraint, Direction, Layout},
   style::{Color, Modifier, Style},
   text::{Span, Spans},
-  widgets::{Block, Borders, Tabs, List, ListItem,ListState},
+  widgets::{Block, Borders, Tabs, List, ListItem},//ListState, Paragraph, Wrap},
   Frame, Terminal,
 };
 
 
-struct StatefulList<T> {
-  state: ListState,
-  items: Vec<T>,
-}
-
-impl<T> StatefulList<T> {
-
-  fn push (&mut self, item : T){
-    self.items.push(item);
-  } 
-
-  fn with_items(items: Vec<T>) -> StatefulList<T> {
-      StatefulList {
-          state: ListState::default(),
-          items,
-      }
-  }
-
-  fn next(&mut self) {
-    if self.items.len() == 0{
-      return;
-    }
-      let i = match self.state.selected() {
-          Some(i) => {
-              if i >= self.items.len() - 1 {
-                  0
-              } else {
-                  i + 1
-              }
-          }
-          None => 0,
-      };
-      self.state.select(Some(i));
-  }
-
-  fn previous(&mut self) {
-      if self.items.len() == 0{
-        return;
-      }
-      let i = match self.state.selected() {
-          Some(i) => {
-              if i == 0 {
-                  self.items.len() - 1
-              } else {
-                  i - 1
-              }
-          }
-          None => 0,
-      };
-      self.state.select(Some(i));
-  }
-
-  fn unselect(&mut self) {
-      self.state.select(None);
-  }
-}
-
-
 struct VisualizatorApp<'a> {
-  pub titles: Vec<&'a str>,
-  pub index: usize,
+  pub tab_titles: Vec<&'a str>,
+  pub tab_index: usize,
 
   pub domain_participant : DomainParticipant,
   pub ros_participant : RosParticipant,
   pub ros_node : RosNode,
 
-  pub topic_list_items : StatefulList<(ListItem<'a>)>
+  pub topic_list_items : Vec<DiscoveredTopicData>,
+  pub external_nodes : Vec<NodeInfo>,
+  pub local_nodes : Vec<NodeInfo>,
+  //viewed datas:
+  pub topic_list_display_items : StatefulList<ListItem<'a>>,
+  pub external_nodes_display_items : StatefulList<ListItem<'a>>,
+  pub local_nodes_display_items : StatefulList<ListItem<'a>>,
 }
 
 impl<'a> VisualizatorApp<'a> {
   fn new( domain_participant: DomainParticipant, ros_participant : RosParticipant, ros_node : RosNode) -> VisualizatorApp<'a> {
     VisualizatorApp {
-          titles: vec!["Participants", "Topics",],
-          index: 0,
+          tab_titles: vec!["Participants", "Local Nodes", "Exteral Nodes",  "Topics",],
+          tab_index: 0,
           domain_participant,
           ros_participant,
           ros_node,
-          topic_list_items : StatefulList::with_items(vec!())
+          external_nodes : vec!(),
+          topic_list_items : vec!(),
+          local_nodes : vec!(),
+          topic_list_display_items : StatefulList::with_items(vec!()),
+          external_nodes_display_items : StatefulList::with_items(vec!()),
+          local_nodes_display_items : StatefulList::with_items(vec!()),
       }
   }
 
-  pub fn next(&mut self) {
-      self.index = (self.index + 1) % self.titles.len();
+  pub fn next_tab(&mut self) {
+      self.tab_index = (self.tab_index + 1) % self.tab_titles.len();
   }
 
-  pub fn previous(&mut self) {
-      if self.index > 0 {
-          self.index -= 1;
+  pub fn previous_tab(&mut self) {
+      if self.tab_index > 0 {
+          self.tab_index -= 1;
       } else {
-          self.index = self.titles.len() - 1;
+          self.tab_index = self.tab_titles.len() - 1;
       }
   }
+
+  pub fn set_topic_list_items(&mut self){
+    self.topic_list_items = self.ros_participant.discovered_topics().clone();
+    let topic_strings = get_topics_list_view_strings(&self.topic_list_items);
+    let previous_state = self.topic_list_display_items.state.clone();
+    self.topic_list_display_items = StatefulList::with_items(vec!());
+    for string in topic_strings{
+      self.topic_list_display_items.push(ListItem::new(string));
+    }
+    self.topic_list_display_items.state = previous_state;
+  }
+
+  pub fn set_node_list_items(&mut self) {
+    let externals = self.ros_participant.get_all_discovered_external_ros_node_infos();
+    let locals = self.ros_participant.get_all_discovered_local_ros_node_infos();
+
+    
+    //let extarnal_node_infos = externals.into_iter().map(|(gid,nodes)|nodes).collect::<NodeInfo>();
+
+    self.local_nodes = locals.clone().into_iter().map(|(string,nodes)|nodes).collect();
+    let external_node_list_string = get_node_list_strings(&self.external_nodes);
+    let local_node_list_string = get_node_list_strings(&self.local_nodes);
+    let previous_state = self.external_nodes_display_items.state.clone();
+    self.external_nodes_display_items = StatefulList::with_items(vec!());
+    
+    for string in external_node_list_string {
+      self.external_nodes_display_items.push(ListItem::new(string));
+    }
+
+    self.local_nodes_display_items = StatefulList::with_items(vec!());
+    for string in local_node_list_string {
+      self.local_nodes_display_items.push(ListItem::new(string));
+    }
+
+  }
+
+}
+
+fn handle_user_input(app: &mut VisualizatorApp, timeout : &Duration) -> bool {
+  let mut quit_application = false;
+  if crossterm::event::poll(*timeout).unwrap() {
+    if let Event::Key(key) = event::read().unwrap() {
+        match key.code {
+            KeyCode::Char('q') => quit_application = true,
+            KeyCode::Right => app.next_tab(),
+            KeyCode::Left => app.previous_tab(),
+            KeyCode::Up => {
+              match app.tab_index {
+                0 => {},
+                1 => {},
+                2 => {},
+                3 => {app.topic_list_display_items.previous()},
+                _ => {},
+              }
+
+            }, 
+            KeyCode::Down => {
+              match app.tab_index {
+                0 => {},
+                1 => {},
+                2 => {},
+                3 => {app.topic_list_display_items.next()},
+                _ => {},
+              }
+            },
+            _ => {}
+        }
+    }
+  }
+  quit_application
 }
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: VisualizatorApp, tick_rate: Duration) -> io::Result<()> {
   let mut last_tick = Instant::now();
+  app.ros_participant.clear();
   loop {
-      app.ros_participant.clear();
+      
       app.ros_participant.handle_node_read();
-
+      app.set_topic_list_items();
 
       terminal.draw(|f| ui(f, &mut app))?;
 
+      let timeout = tick_rate.checked_sub(last_tick.elapsed()).unwrap_or_else(|| Duration::from_secs(0));
 
-      let timeout = tick_rate
-            .checked_sub(last_tick.elapsed())
-            .unwrap_or_else(|| Duration::from_secs(0));
-        if crossterm::event::poll(timeout)? {
-            if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Char('q') => return Ok(()),
-                    KeyCode::Right => app.next(),
-                    KeyCode::Left => app.previous(),
-                    KeyCode::Up => app.topic_list_items.previous(),
-                    KeyCode::Down => app.topic_list_items.next(),
-                    _ => {}
-                }
-            }
-        }
-        
-        if last_tick.elapsed() >= tick_rate {
-            last_tick = Instant::now();
-        }
-
-
-      /*
-      if let Event::Key(key) = event::read()? {
-          match key.code {
-              KeyCode::Char('q') => return Ok(()),
-              KeyCode::Right => app.next(),
-              KeyCode::Left => app.previous(),
-              _ => {}
-          }
+      if handle_user_input(&mut app, &timeout)
+      {
+        return Ok(());
       }
-      */
+        
+      if last_tick.elapsed() >= tick_rate {
+        last_tick = Instant::now();
+      }
   }
 }
 
@@ -206,13 +223,24 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut VisualizatorApp) {
   let chunks = Layout::default()
       .direction(Direction::Vertical)
       .margin(5)
-      .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
+      .constraints([Constraint::Length(5),
+                     Constraint::Length(3),
+                     Constraint::Length(10)
+                     ].as_ref())
       .split(size);
 
   let block = Block::default().style(Style::default().bg(Color::Black).fg(Color::White));
   f.render_widget(block, size);
+
+  let help_strings = vec!("Change tab with arrow left and arrow right buttons.".to_string(),
+    "Change selected item with arrow up and arrow down buttons".to_string(),
+    "Quit application with 'q' button.".to_string(),);
+  let user_help  = create_paragraph_from_string_list(help_strings," Usage Instructions".to_string());
+  
+  f.render_widget(user_help, chunks[0]);
+
   let titles = app
-      .titles
+      .tab_titles
       .iter()
       .map(|t| {
           let (first, rest) = t.split_at(1);
@@ -224,37 +252,37 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut VisualizatorApp) {
       .collect();
   let tabs = Tabs::new(titles)
       .block(Block::default().borders(Borders::ALL).title("Tabs"))
-      .select(app.index)
+      .select(app.tab_index)
       .style(Style::default().fg(Color::Cyan))
       .highlight_style(
           Style::default()
               .add_modifier(Modifier::BOLD)
               .bg(Color::Black),
       );
-  f.render_widget(tabs, chunks[0]);
 
   
 
-  let topic_strings = get_topics_view_strings(&app.ros_participant);
+  f.render_widget(tabs, chunks[1]);
 
-  //DOTO CLEAN LIST ?
-  let previous_state = app.topic_list_items.state.clone();
-  app.topic_list_items = StatefulList::with_items(vec!());
-  for string in topic_strings{
-    let new_item = ListItem::new(string.clone());
-    if app.topic_list_items.items.iter().any(|x| x == &new_item){
-      //ALREADY IN List
-    }else{
-      app.topic_list_items.push(ListItem::new(string));
-    }
-  }
-  app.topic_list_items.state = previous_state;
+  let first_row = create_layput_row(chunks[2]);
 
-  let list_of_topics = List::new(app.topic_list_items.items.clone())
+  let list_of_topics = List::new(app.topic_list_display_items.items.clone())
     .block(Block::default().title("Topics").borders(Borders::ALL))
     .style(Style::default().fg(Color::White))
     .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
     .highlight_symbol(">>");
+
+  let list_of_local_nodes = List::new(app.local_nodes_display_items.items.clone())
+  .block(Block::default().title("Local Nodes").borders(Borders::ALL))
+  .style(Style::default().fg(Color::White))
+  .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+  .highlight_symbol(">>");
+
+  let list_of_external_nodes = List::new(app.external_nodes_display_items.items.clone())
+  .block(Block::default().title("External Nodes").borders(Borders::ALL))
+  .style(Style::default().fg(Color::White))
+  .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+  .highlight_symbol(">>");
 
   let participant_strings = get_external_node_info_strings(&app.ros_participant);
   let local_participant_strings = get_local_ros_participant_info_strings(&app.ros_participant);
@@ -266,17 +294,35 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut VisualizatorApp) {
     participant_list_items.push(ListItem::new(string));
   }
 
+  let selected_topic_strings = match app.topic_list_display_items.state.selected(){
+    Some (index) =>{
+      get_topic_view_strings(&app.ros_participant, app.topic_list_items[index].topic_name())
+    }
+    None =>{vec!()}
+  };
+
+  let selected_topic_paragraph = create_paragraph_from_string_list(selected_topic_strings,"Topic information".to_string());
+  
+
   let list_of_participants = List::new(participant_list_items)
   .block(Block::default().title("Participants").borders(Borders::ALL))
   .style(Style::default().fg(Color::White))
   .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
   .highlight_symbol(">>");
 
-  match app.index{
-    0 => {f.render_widget(list_of_participants, chunks[1]);},
-    1 => {f.render_stateful_widget(list_of_topics, chunks[1],&mut app.topic_list_items.state);}
-    2 => {},
-    3 => {},
+  match app.tab_index{
+    0 => {f.render_widget(list_of_participants, first_row[0]);},
+    1 => {
+      f.render_stateful_widget(list_of_local_nodes, first_row[0], &mut app.local_nodes_display_items.state);
+    }
+    2 => {
+      f.render_stateful_widget(list_of_external_nodes, first_row[0], &mut app.local_nodes_display_items.state);
+    },
+    3 => {
+      f.render_stateful_widget(list_of_topics, first_row[0], &mut app.topic_list_display_items.state);
+      f.render_widget(selected_topic_paragraph, first_row[1]);
+    },
+    4 => {},
     _ => unreachable!(),
   };
 
@@ -322,13 +368,13 @@ fn main()  -> Result<(), Box<dyn Error>>  {
   let domain_participant = DomainParticipant::new(domain_id)
     .unwrap_or_else(|e| panic!("DomainParticipant construction failed: {:?}", e));
     
-  let mut ros_participant = RosParticipant::new().unwrap();
+  let ros_participant = RosParticipant::new().unwrap();
   let ros_node = ros_participant.new_ros_node("local_ node", "/ros2_demo", NodeOptions::new(true)).unwrap();
   
-  let mut visualizor_app = VisualizatorApp::new(domain_participant,ros_participant,ros_node);
+  let visualizor_app = VisualizatorApp::new(domain_participant,ros_participant,ros_node);
 
   let tick_rate = Duration::from_millis(250);
-  let res = run_app(&mut terminal, visualizor_app,tick_rate);
+  let _res = run_app(&mut terminal, visualizor_app,tick_rate);
 
   disable_raw_mode()?;
   execute!(
