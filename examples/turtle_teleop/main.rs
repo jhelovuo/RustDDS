7669e638dd9bca67b962d3b8ffe17de7520c23dc
Juhana Helovuo <juhana.helovuo@atostek.com>
2021-12-16 11:30:55 +0200

    Move QoS from crate top level back to submodule to avoid excessively large top level.

--- examples/turtle_teleop/main.rs
+++ examples/turtle_teleop/main.rs
@@ -8,17 +8,11 @@ use log::{debug, error, info, warn};
 use serde::{Deserialize, Serialize};
 use mio::{Events, Poll, PollOpt, Ready, Token};
 use mio_extras::channel as mio_channel;
-use rustdds::{
-  dds::{
-    qos::{
-      policy::{Durability, History, Liveliness, Reliability},
-      QosPolicies, QosPolicyBuilder,
-    },
-    TopicKind,
-  },
-  ros2::{NodeOptions, RosParticipant},
-  serialization::{CDRDeserializerAdapter, CDRSerializerAdapter},
-};
+
+use rustdds::*;
+use rustdds::policy::*;
+use rustdds::ros2::{NodeOptions, RosParticipant};
+
 use ui::{RosCommand, UiController};
 
 // modules
