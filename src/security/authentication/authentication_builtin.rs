8539da8cefe994a875aa427340640b4acb56cecc
Olli Huopio <olli.huopio@atostek.com>
2023-05-08 17:50:20 +0300

    Add function signatures to authentication interface

--- src/security/authentication/authentication_builtin.rs
+++ src/security/authentication/authentication_builtin.rs
@@ -1,4 +1,8 @@
-use crate::{dds::qos::QosPolicies, security::SecurityResult, structure::guid::GUID};
+use crate::{
+  dds::qos::QosPolicies,
+  security::{access_control::*, SecurityResult},
+  structure::guid::GUID,
+};
 use super::*;
 
 // A struct implementing the built-in Authentication plugin
@@ -18,4 +22,89 @@ impl AuthenticationBuiltIn {
   ) -> SecurityResult<ValidationOutcome> {
     todo!();
   }
+
+  pub fn get_identity_token(&self, handle: IdentityHandle) -> SecurityResult<IdentityToken> {
+    todo!();
+  }
+
+  pub fn get_identity_status_token(
+    &self,
+    handle: IdentityHandle,
+  ) -> SecurityResult<IdentityStatusToken> {
+    todo!();
+  }
+
+  pub fn set_permissions_credential_and_token(
+    &self,
+    handle: IdentityHandle,
+    permissions_credential_token: PermissionsCredentialToken,
+    permissions_token: PermissionsToken,
+  ) -> SecurityResult<()> {
+    todo!();
+  }
+
+  pub fn validate_remote_identity(
+    &self,
+    remote_identity_handle: &mut IdentityHandle,
+    local_auth_request_token: &mut AuthRequestMessageToken,
+    remote_auth_request_token: AuthRequestMessageToken,
+    local_identity_handle: IdentityHandle,
+    remote_identity_token: IdentityToken,
+    remote_participant_guid: GUID,
+  ) -> SecurityResult<ValidationOutcome> {
+    todo!();
+  }
+
+  pub fn begin_handshake_request(
+    &self,
+    handshake_handle: &mut HandshakeHandle,
+    handshake_message: &mut HandshakeMessageToken,
+    initiator_identity_handle: IdentityHandle,
+    replier_identity_handle: IdentityHandle,
+    serialized_local_participant_data: Vec<u8>,
+  ) -> SecurityResult<ValidationOutcome> {
+    todo!();
+  }
+
+  pub fn begin_handshake_reply(
+    &self,
+    handshake_handle: &mut HandshakeHandle,
+    handshake_message_out: &mut HandshakeMessageToken,
+    handshake_message_in: HandshakeMessageToken,
+    initiator_identity_handle: IdentityHandle,
+    replier_identity_handle: IdentityHandle,
+    serialized_local_participant_data: Vec<u8>,
+  ) -> SecurityResult<ValidationOutcome> {
+    todo!();
+  }
+
+  pub fn process_handshake(
+    &self,
+    handshake_message_out: &mut HandshakeMessageToken,
+    handshake_message_in: HandshakeMessageToken,
+    handshake_handle: HandshakeHandle,
+  ) -> SecurityResult<ValidationOutcome> {
+    todo!();
+  }
+
+  pub fn get_shared_secret(
+    &self,
+    handshake_handle: HandshakeHandle,
+  ) -> SecurityResult<SharedSecretHandle> {
+    todo!();
+  }
+
+  pub fn get_authenticated_peer_credential_token(
+    &self,
+    handshake_handle: HandshakeHandle,
+  ) -> SecurityResult<AuthenticatedPeerCredentialToken> {
+    todo!();
+  }
+
+  pub fn set_listener(&self) -> SecurityResult<()> {
+    todo!();
+  }
+
+  // TODO: Can the different return methods (e.g. return_identity_token) be left
+  // out, since Rust manages memory for us?
 }
