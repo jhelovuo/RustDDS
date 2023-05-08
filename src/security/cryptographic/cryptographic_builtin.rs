6449ca6e7164251aa5323d67ea445d2a251b5b3d
Olli Huopio <olli.huopio@atostek.com>
2023-05-08 16:08:26 +0300

    Make the plugin structs public

--- src/security/cryptographic/cryptographic_builtin.rs
+++ src/security/cryptographic/cryptographic_builtin.rs
@@ -1,5 +1,5 @@
 // A struct implementing the built-in Authentication plugin
 // See sections 8.5 and 9.5 of the Security specification (v. 1.1)
-struct CryptographicBuiltIn {
+pub struct CryptographicBuiltIn {
   todo: String,
 }
