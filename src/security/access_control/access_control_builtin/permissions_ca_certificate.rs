4f3b20a315fc69e3cd8bfaabaf418213f671969e
Juhana Helovuo <juhana.helovuo@atostek.com>
2023-08-15 09:30:59 +0300

    Add S/MIME parsing and signature vereification for config files. RUS-71

--- src/security/access_control/access_control_builtin/permissions_ca_certificate.rs
+++ src/security/access_control/access_control_builtin/permissions_ca_certificate.rs
@@ -13,3 +13,46 @@
 // Table 56."
 //
 
+use x509_certificate::certificate::{CapturedX509Certificate};
+
+use super::domain_participant_permissions_document::{ConfigError, to_config_error, };
+
+
+#[derive(Debug)]
+struct Certificate {
+  cert: CapturedX509Certificate,
+}
+
+impl Certificate {
+  pub fn from_pem(pem_data: impl AsRef<[u8]>) -> Result<Self, ConfigError> {
+    let cert = CapturedX509Certificate::from_pem(pem_data)
+      .map_err(|e| to_config_error("Cannot read X.509 Certificate",e) ) ?;
+
+    Ok(Certificate{ cert })
+  }
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  
+  #[test]
+  pub fn parse_example() {
+    let cert_pem = r#"-----BEGIN CERTIFICATE-----
+MIIBOzCB4qADAgECAhR361786/qVPfJWWDw4Wg5cmJUwBTAKBggqhkjOPQQDAjAS
+MRAwDgYDVQQDDAdzcm9zMkNBMB4XDTIzMDcyMzA4MjgzNloXDTMzMDcyMTA4Mjgz
+NlowEjEQMA4GA1UEAwwHc3JvczJDQTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IA
+BMpvJQ/91ZqnmRRteTL2qaEFz2d7SGAQQk9PIhhZCV1tlLwYf/hI4xWLJaEv8FxJ
+TjxXRGJ1U+/IqqqIvJVpWaSjFjAUMBIGA1UdEwEB/wQIMAYBAf8CAQEwCgYIKoZI
+zj0EAwIDSAAwRQIgEiyVGRc664+/TE/HImA4WNwsSi/alHqPYB58BWINj34CIQDD
+iHhbVPRB9Uxts9CwglxYgZoUdGUAxreYIIaLO4yLqw==
+-----END CERTIFICATE-----
+"#;
+
+    let cert = Certificate::from_pem(cert_pem).unwrap();
+
+    println!("{:?}",cert);
+
+  }
+}
