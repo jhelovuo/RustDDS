28ed26f8f945d333f5554642608af11060b5ac97
Juhana Helovuo <juhana.helovuo@atostek.com>
2024-02-08 11:07:08 +0200

    Remove useless file.

--- src/dds/dds_entity.rs
+++
@@ -1,6 +0,0 @@
-use crate::dds::qos::HasQoSPolicy;
-
-// Intended to somewhat describe DDS 2.2.2.1.1 Entity Class
-// pub trait DDSEntity: HasQoSPolicy {}
-
-// But removed, because no-one is using it.
